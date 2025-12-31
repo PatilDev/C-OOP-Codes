1152089f14ab24185a302dc643a5bee1f9a2f0a1
Devendra Yuvaraj Patil <112844371+PatilDev@users.noreply.github.com>
2025-12-31 22:56:58 +0530

    Polymorphism demo code

---
+++ Polymorphism/Polymorphism/MainProgram.cs
@@ -0,0 +1,12 @@
+﻿using Polymorphism;
+
+public class MainProgram
+{
+    public static void Main(String [] args)
+    {
+
+        Console.WriteLine("Method ");
+        MehtodOverloading mo = new MehtodOverloading();
+        mo.add();
+    }
+}
---
+++ Polymorphism/Polymorphism/MehtodOverloading.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Channels;
+
+namespace Polymorphism
+{
+    internal class MehtodOverloading
+    {
+        public void add(int a,int b)
+        {
+            Console.WriteLine("the sum is", a+b);
+        }
+
+        public void add(string a,string b)
+        {
+            Console.WriteLine( "your Concatination String",a+b);
+        }
+
+        public void add(int a,int b,int c)
+        {
+            Console.WriteLine("Sum of 3 integers is ", a + b + c);
+        }
+    }
+}
