cef8d82a4131f8e7fb84defb7e8d85935c04505e
Devendra Yuvaraj Patil <112844371+PatilDev@users.noreply.github.com>
2025-12-31 22:55:51 +0530

    Add files via upload
    Encapsulation in c#

---
+++ Encapsulation/Encapsulation/EncapsulationClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encapsulation
+{
+    internal class EncapsulationClass
+    {
+        public string name { get; set; }
+
+        public string location {  get; set; }
+
+        public int salary { get; set; }
+
+        
+    }
+}
---
+++ Encapsulation/Encapsulation/MainClass.cs
@@ -0,0 +1,29 @@
+﻿using Encapsulation;
+using System.ComponentModel;
+
+public class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the following Details");
+        Console.Write("Employye Name\t");
+        string empName = Console.ReadLine();
+
+        Console.WriteLine("EmployeeLocation\t");
+        string empLocation = Console.ReadLine();
+
+        Console.WriteLine("salary ");
+        int empSalary=Convert.ToInt32(Console.ReadLine());
+        EncapsulationClass encapsulation = new EncapsulationClass()
+       {
+            name = empName,
+            location = empLocation,
+            salary = empSalary
+        };
+
+        Console.WriteLine("Employee name :-  " +encapsulation.name+
+            "\nEmployee salary:-"+ encapsulation.salary+
+            "\nEmployye Location "+encapsulation.location);
+
+    }
+}
